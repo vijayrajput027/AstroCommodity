8614c7ace5f3e54cd52551f2e7f14a20707b2b11
vijayrajput027 <er.vijayrajput@gmail.com>
2016-01-16 16:04:41 +0530

    second commit

--- app/src/main/java/com/mohanastrology/commodity/LuckImprovementActivity.java
+++ app/src/main/java/com/mohanastrology/commodity/LuckImprovementActivity.java
@@ -4,15 +4,12 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.mohanastrology.commodity.adapter.CommodityAdapter;
@@ -43,7 +40,7 @@ public class LuckImprovementActivity extends AppCompatActivity implements Commod
         setContentView(R.layout.activity_luck_improvement);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Luck");
+        getSupportActionBar().setTitle("Luck Improvement");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
