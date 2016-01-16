cffa00ff59bfef664aa6a42a8a641549bd5e235a
vijayrajput027 <er.vijayrajput@gmail.com>
2016-01-16 16:24:08 +0530

    16/01/2016

--- app/src/main/java/com/mohanastrology/commodity/VastuActivity.java
+++ app/src/main/java/com/mohanastrology/commodity/VastuActivity.java
@@ -4,15 +4,12 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.mohanastrology.commodity.adapter.CommodityAdapter;
@@ -117,7 +114,7 @@ public class VastuActivity extends AppCompatActivity implements CommodityAdapter
             }
             if (result.equals("1")) {
                 adapter.notifyDataSetChanged();
-                Toast.makeText(getBaseContext(), "Successfully..", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(getBaseContext(), "Successfully..", Toast.LENGTH_SHORT).show();
 
             } else {
                 Toast.makeText(getBaseContext(), "Failed..", Toast.LENGTH_SHORT).show();
